6ef7c34d3fff8179d61f5d00bef291b89b485450
[dyad] <robintardieu92340@hotmail.fr>
2026-01-02 17:38:08 +0100

    [dyad] wrote 2 file(s)

--- src/components/SavedNumberSelectItem.tsx
+++ src/components/SavedNumberSelectItem.tsx
@@ -8,7 +8,10 @@ interface SavedNumberSelectItemProps {
 
 const SavedNumberSelectItem: React.FC<SavedNumberSelectItemProps> = ({ num }) => {
   return (
-    <SelectItem value={num.number} className="pl-4 pr-4">
+    <SelectItem 
+      value={num.number} 
+      className="pl-4 pr-4 data-[state=checked]:bg-accent data-[state=checked]:text-foreground [&>span:last-child]:hidden"
+    >
       <span className="truncate text-sm">
         {num.name} ({num.number})
       </span>
